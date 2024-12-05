31e053044bd005c48e107627e0edc7ed8cef8a81
shubhamRavani <shubhamravani67890@gmail.com>
2024-12-06 01:56:39 +0530

    refactor(c-program): add if-else program in 'conditionals-example'

---
+++ conditionals-example/if-else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    if (a > b)
+    {
+        printf("a is greater than b\n");
+    }
+    else
+    {
+        printf("b is greater than a\n");
+    }
+
+    return 0;
+}
